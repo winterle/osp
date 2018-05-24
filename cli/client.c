324e897f63edb4b3fd508e78c67fb2aa5e25293e
PhexSerath <34057569+PhexSerath@users.noreply.github.com>
2018-05-24 23:19:32 +0200

    Basic

--- cli/client.c
+++ cli/client.c
@@ -1,13 +1,65 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "client.h"
 
 #define PORT 9000
 #define HOST "127.0.0.1"
 
+
+int create_socket(){
+	int connector = socket(AF_INET, SOCK_STREAM, 0);
+	if(connector<0) {
+		die("Can not create a socket.");
+		return -1;
+	}else{
+		return connector;
+	}
+}
+
 int main()
 {
-	getc(stdin);
+	/*int sys = system ("/bin/stty raw");
+	if(sys==-1){
+		printf("dont work.\n");
+	}*/
+	struct sockaddr_in addr = {
+			.sin_family = AF_INET,
+			.sin_port = htons(PORT),
+			.sin_addr.s_addr = inet_addr(HOST)
+	};
+
+	char lineBuffer[256];
+	char buf[256];
+	int cli;
+
+	for (int j = 0; j < 256; ++j) {
+		lineBuffer[j]='\0';
+	}
+
+	cli = create_socket();
+
+	if(connect(cli,(struct sockaddr*) &addr, sizeof(addr))< 0){
+		die("Can not connect to socket");
+	}
+
+	while(strcmp(lineBuffer,"logout")!=0){
+		char c;
+		int i =0;
+
+		while((c=getchar())!='\n'&&i!=256){
+			lineBuffer[i]=c;
+			i++;
+		}
+        lineBuffer[i]=10;
+		if(write(cli, lineBuffer, 256)<0){
+			die("Can not send msg.\n");
+		}
+
+		if(read(cli, buf, sizeof(buf))<0){
+			die("Can not receive msg.\n");
+		}
+		printf("[recv] %s\n", buf);
+	}
+	close(cli);
 	printf("Client Exit\n");
-	
+	exit(0);
 	return 0;
 }
--- srv/server.c
+++ srv/server.c
@@ -1,16 +1,93 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "server.h"
 
 #define PORT 9000
 
+static int split(int* fd){
+
+    int pfd[2][2];
+    pipe(pfd[0]);
+    pipe(pfd[1]);
+
+
+	switch (fork()){
+		case 0:
+            close(pfd[0][1]);
+            close(pfd[1][0]);
+            fd[0]= pfd[0][0];
+            fd[1]= pfd[1][1];
+            return 0;
+
+		default:
+            close(pfd[1][1]);
+            close(pfd[0][0]);
+            fd[0]= pfd[1][0];
+            fd[1]= pfd[0][1];
+            return 1;
+	}
+}
+
+static void start_shell(int * fd, int cfd){
+
+    switch (split(fd)){
+        case 0:
+            dup2(fd[0],STDIN_FILENO);
+            dup2(STDOUT_FILENO,fd[1]);
+            execlp("./shell", "shell", NULL);
+            exit(-1);
+
+        case 1:
+            dup2(fd[0],cfd);
+            dup2(cfd,fd[1]);
+
+    }
+}
+
+
 int main()
 {
-	while (1)
+	srv_addr.sin_family = AF_INET;
+	srv_addr.sin_port=htons(PORT);
+	srv_addr.sin_addr.s_addr = INADDR_ANY;
+
+	if ((sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		 die("Couldn't open the socket");
+	if (bind(sfd, (struct sockaddr*) &srv_addr, sad_sz) < 0)
+		 die("Couldn't bind socket");
+	if (listen(sfd, 1) < 0)
+		 die("Couldn't listen to the socket");
+	cfd = accept(sfd, (struct sockaddr*) &cli_addr, &sad_sz);
+	if (cfd < 0)
+		die("Couldn't accept incoming connection");
+	//buf->stdin
+	//stdout->client
+    int pipePtr[2];
+
+    start_shell(pipePtr, cfd);
+	while ((bytes = read(cfd, buf, sizeof(buf)))) {
+		if (bytes < 0) {
+            die("Couldn't receive message");
+        }
+		    if(write(pipePtr[0], buf, bytes)<0) {
+                die("P0 dont work");
+                close(cfd); close(sfd);
+                exit(-1);
+            }
+		    if(read(pipePtr[1], buf, sizeof(buf))<0) {
+                die("P1 dont work");
+                close(cfd); close(sfd);
+                exit(-1);
+            }
+
+		if (write(cfd, buf, bytes) < 0)
+			die("Couldn't send message");
+	}
+	close(cfd); close(sfd);
+
+	/*while (1)
 	{
 		printf("[srv]: idle\n");
 		sleep(1);
 	}
-	
+	*/
 	return 0;
 }
